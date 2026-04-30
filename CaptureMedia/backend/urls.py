4468cf3a93eb5f9bec13eb2b913b9633fbd565b2
meheza_studer <meheza@gmail.com>
2026-04-30 03:18:23 +0000

    integration de template avec une page index , un page login , une page signUp et la derniere forgot , fait le 30 avril

---
+++ CaptureMedia/backend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from . import views
+urlpatterns = [
+    path('forgot' , views.ForgotPage , name="forgot"),
+    path('' , views.IndexPage , name="index"),
+    path('login' , views.LoginPage , name="login"),
+    path('signUp' , views.Sign_upPage , name="signUp"),
+]
---
+++ CaptureMedia/backend/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def ForgotPage (request):
+    return render (request , "forgot.html")
+
+def IndexPage (request):
+    return render (request , "index.html")
+
+def LoginPage(request):
+    return render (request , "login.html")
+
+def Sign_upPage(request):
+    return render (request ,"sign-up.html")
