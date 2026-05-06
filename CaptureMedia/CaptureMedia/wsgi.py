d3e44cdb89b9ce4f6c81ba34775345357d17c9eb
paul25012004 <paulkoumadi1@gmail.com>
2026-05-06 09:06:44 +0000

    gestion des articles

---
+++ CaptureMedia/CaptureMedia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CaptureMedia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CaptureMedia.settings')
+
+application = get_wsgi_application()
---
+++ CaptureMedia/backend/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def ForgotPage (request):
+    return render (request , "forgot.html")
+
+def IndexPage (request):
+    return render (request , "index.html")
+
+def LoginPage(request):
+    return render (request , "login.html")
+
+def Sign_upPage(request):
+    return render (request ,"sign-up.html")
+
+def AddAnnoncePage(request):
+    return render(request, "add-annonce.html")
